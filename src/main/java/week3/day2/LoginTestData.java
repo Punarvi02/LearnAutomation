3e0fca20930ac7ae742671d692c5d51e8ba6a865
Punarvi02 <karanampunarvi@gmail.com>
2025-08-10 22:18:20 +0800

    commit on 10th August

---
+++ src/main/java/week3/day2/LoginTestData.java
@@ -0,0 +1,24 @@
+package week3.day2;
+
+public class LoginTestData extends TestData{
+	
+	public void enterUsername() {
+		System.out.println("Username entered");
+	}
+	
+	public void enterPassword() {
+		System.out.println("Password entered");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		LoginTestData test = new LoginTestData();
+		test.enterUsername();
+		test.enterPassword();
+		test.enterCredentials();
+		test.navigateToHomePage();
+
+	}
+
+}
---
+++ src/main/java/week3/day2/TestData.java
@@ -0,0 +1,16 @@
+package week3.day2;
+
+public class TestData {
+	
+	public void enterCredentials() {
+		System.out.println("Credentials entered");
+	}
+	
+	public void navigateToHomePage() {
+		System.out.println("Navigated to Homepage");
+		
+		
+	}
+	
+
+}
